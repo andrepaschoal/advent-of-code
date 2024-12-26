ea2db774b725b4b47d625d37b26657f34daba5e9
andrepaschoal-dh <andre.paschoal@deliveryhero.com>
2024-12-26 23:28:04 +0100

    Implement DistanceCalculator

--- app/src/main/kotlin/day01/DistanceCalculator.kt
+++ app/src/main/kotlin/day01/DistanceCalculator.kt
@@ -1,5 +1,22 @@
 package day01
 
 class DistanceCalculator {
-    fun calculate(arrayOf: Array<String>) = 11
+    fun calculate(input: Array<String>): Int {
+
+        val parsedInput = InputListParser().parse(input)
+        val left = parsedInput.first
+        val right = parsedInput.second
+
+        return left.mapIndexed{
+            index, leftValue ->
+
+            val rightValue = right[index]
+            var diff = leftValue - rightValue
+
+            if (diff < 0)
+                diff *= -1
+
+            diff
+        }.sum()
+    }
 }
